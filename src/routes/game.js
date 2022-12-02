701a1acf0152d7442f85ed018c9f56c79e3810b1
Filipe Câncio <filipe.cancio@gmail.com>
2022-12-02 02:46:41 -0300

    configurando o jogo

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,11 +1,26 @@
-import { Router, response } from 'express'
+import { Router, response, urlencoded } from 'express'
 import {getRandomPokemon} from '../server/pokeapi.js'
-import circularJSON from 'circular-json'
 
 export const gameRoutes = Router();
 
 gameRoutes.get("/", async (req, res) =>{
     const response = await getRandomPokemon()
-    console.log(response.data.count)
-    res.send(response.data)
+    const pokemonList = response.data.results.map(pokemon => {
+        return {
+            "name":pokemon.name,
+            "id": getId(pokemon.url),
+            "avatarUrl": getAvatar(pokemon.url)
+        }
+    })
+    console.log(response.data.results)
+    res.send(pokemonList)
 })
+
+const getId = (url)=> {
+    return url.replace("https://pokeapi.co/api/v2/pokemon-species/", "").replace("/","");
+}
+
+const getAvatar = (url)=> {
+    const id = getId(url)
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png` 
+}
